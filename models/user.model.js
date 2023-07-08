a318261fba4a71135084803fa64c6fb15619b1a2
DestructiveClub <intelligentson17@gmail.com>
2023-07-08 18:04:25 +0000

    added required property to email

--- models/user.model.js
+++ models/user.model.js
@@ -1,52 +1,54 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const intern = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, "First name is required"]
-    },
-    lastName: {
-        type: String,
-    },
-    otherNames: [String],
-    dob: {
-        type: Date,
-        required: [true, "provide date of birth"]
-    },
-    bio: {
-        type: String,
-    },
-    phoneNumber: {
-        type: String,
-    },
-    email: {
-        type: String,
-    },
-    // skills: [{
-    //     type: mongoose.SchemaTypes.ObjectId,
-    //     ref: "skill"
-    // }],
-    // coureses: [{
-    //     type: mongoose.SchemaTypes.ObjectId,
-    //     ref: "course"
-    // }],
-    level: {
-        type: Number,
-    },
-    password: {
-        type: String,
-        required: [true, "password is required"]
-    },
-})
+  firstName: {
+    type: String,
+    required: [true, "First name is required"],
+  },
+  lastName: {
+    type: String,
+  },
+  otherNames: [String],
+  dob: {
+    type: Date,
+    // required: [true, "provide date of birth"],
+  },
+  bio: {
+    type: String,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  // skills: [{
+  //     type: mongoose.SchemaTypes.ObjectId,
+  //     ref: "skill"
+  // }],
+  // coureses: [{
+  //     type: mongoose.SchemaTypes.ObjectId,
+  //     ref: "course"
+  // }],
+  level: {
+    type: Number,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+});
 
 intern.virtual("fullName").get(function () {
-    let fullname = this.firstName
+  let fullname = this.firstName;
 
-    if (this.otherNames.length !== 0) fullname = fullname + " " + this.otherNames.join(" ")
+  if (this.otherNames.length !== 0)
+    fullname = fullname + " " + this.otherNames.join(" ");
 
-    if (this.lastName) fullname = fullname + " " + this.lastName
+  if (this.lastName) fullname = fullname + " " + this.lastName;
 
-    return fullname
-})
+  return fullname;
+});
 
-module.exports = mongoose.model("intern", intern)
+module.exports = mongoose.model("intern", intern);
