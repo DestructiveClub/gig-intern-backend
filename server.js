84e86816e536e44f5878a003ceea1c65403535fe
DestructiveClub <intelligentson17@gmail.com>
2023-07-08 14:59:58 +0000

    sylvester Asante: Rebuilt the server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const { dbConnect } = require("./config/dbConnect.js");
+
+const port = 5000;
+dotenv.config();
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: "POST",
+//     withCredentials: true,
+//   })
+// );
+
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.listen(port || 4000, () => {
+  dbConnect();
+  console.log(`Server is connected to port ${port || 4000}`);
+});
