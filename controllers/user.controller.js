4f6a87c9bf349c1ffc5fc3dfb720749565c987dc
DestructiveClub <intelligentson17@gmail.com>
2023-07-08 18:03:34 +0000

    Sylvester Asante : Wrote Regiter controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,14 +1,42 @@
-const login = async (req, res) => {
-    
-  res.status(200).json({
-    message: "This is the login controller",
-  });
-};
+const User = require("../models/user.model.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
-  res.status(200).json({
-    message: "This is the register controller",
-  });
+  try {
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      otherNames: req.body.otherNames,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10),
+    });
+
+    const userExists = await User.findOne({ email: req.body.email });
+
+    if (userExists) {
+      return res.status(401).json({
+        message: "User Already Exists",
+      });
+    }
+    // jwt.sign();
+
+    await newUser.save();
+    res.status(200).json({
+      user: newUser,
+      message: "User has been registered successfully",
+    });
+    // res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const logout = async (req, res) => {
