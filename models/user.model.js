bb63131b21c3bf3ff42ea82ab14fb3b1932bec9d
asantesylvesterkwame <intelligentson17@gmail.com>
2023-09-16 11:34:59 +0000

    Asante Sylvester : Added bio Field to User Model

--- models/user.model.js
+++ models/user.model.js
@@ -1,43 +1,47 @@
 const mongoose = require("mongoose");
 
 const user = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, "First name is required"]
-    },
-    lastName: {
-        type: String,
-    },
-    otherNames: [String],
-    phoneNumber: {
-        type: String,
-        unique: [true, "phone number must be unique"]
-    },
-    email: {
-        type: String,
-        unique: [true, "email must be unique"]
-    },
-    password: {
-        type: String,
-        required: [true, "password is required"]
-    },
-})
+  firstName: {
+    type: String,
+    required: [true, "First name is required"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required"],
+  },
+  otherNames: [String],
+  phoneNumber: {
+    type: Number,
+    unique: [true, "phone number must be unique"],
+  },
+  email: {
+    type: String,
+    unique: [true, "email must be unique"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  bio: {
+    type: String,
+  },
+});
 
-class UserClass{
-    get FullName(){
-        const result = this.firstName
-    
-        for (let name of this.otherNames){
-            result = result + " " + name
-        }
-    
-        if(!this.lastName) return result
-    
-        return result + " " + this.lastName
+class UserClass {
+  get FullName() {
+    const result = this.firstName;
+
+    for (let name of this.otherNames) {
+      result = result + " " + name;
     }
+
+    if (!this.lastName) return result;
+
+    return result + " " + this.lastName;
+  }
 }
 
 module.exports = {
-    user,
-    UserClass,
-}
+  user,
+  UserClass,
+};
